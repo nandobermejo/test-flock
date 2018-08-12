09ec16346ebcc08542e1ed3c130dd987ccfe99f2
nando <info@nandobermejo.com>
2018-08-12 10:45:05 +0100

    Add Redis dependency and implemented redis cache for the API endpoints

--- routes/drone.js
+++ routes/drone.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const redis = require('redis');
+const client = redis.createClient(process.env.PRIVATE_REDIS_PORT,process.env.PRIVATE_REDIS_URL);
 
-router.get('/:id', (req, res, next) => {
-  axios.get(process.env.API_URL +'/drone/' + req.params.id)
-  .then((response) => res.send(response.data))
+const cache = (req, res, next) => {
+  client.get(`test_drone_${req.params.id}`, function (err, data) {
+    if (err) next();
+
+    if (data) res.send(JSON.parse(data));
+    else next();
+  });
+}
+
+router.get('/:id', cache, (req, res, next) => {
+  axios.get(`${process.env.API_URL}/drone/${req.params.id}`)
+  .then((response) => {
+    client.setex(`test_drone_${req.params.id}`, 60, JSON.stringify(response.data));
+    res.send(response.data)
+  })
   .catch((err) => next(err))
 });
 
--- routes/drones.js
+++ routes/drones.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const redis = require('redis');
+const client = redis.createClient(process.env.PRIVATE_REDIS_PORT,process.env.PRIVATE_REDIS_URL);
 
-router.get('', (req, res, next) => {
-  axios.get(process.env.API_URL +'/drones')
-  .then((response) => res.send(response.data))
+const cache = (req, res, next) => {
+  client.get('test_drones', function (err, data) {
+    if (err) next();
+
+    if (data) res.send(JSON.parse(data));
+    else next();
+  });
+}
+
+router.get('', cache, (req, res, next) => {
+  axios.get(`${process.env.API_URL}/drones`)
+  .then((response) => {
+    client.setex('test_drones', 60, JSON.stringify(response.data));
+    res.send(response.data)
+  })
   .catch((err) => next(err))
 });
 
