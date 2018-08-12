49bfc5596ac6540cd3797886beb1df8c789229fa
nando <info@nandobermejo.com>
2018-08-12 10:11:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flock Tech Test
---
+++ main.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: err.message
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Test App listening on port ${process.env.PORT}`)
+});
